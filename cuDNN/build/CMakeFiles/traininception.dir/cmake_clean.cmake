b5006650a28f359b3658abbef6d73e2a764c6cdf
Yikun Wang <yw2227@en-openmpi04.ece.cornell.edu>
2019-05-11 01:12:44 -0400

    auxiliary updated

---
+++ cuDNN/build/CMakeFiles/traininception.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/traininception.dir/./traininception_generated_inception.cu.o"
+  "CMakeFiles/traininception.dir/readubyte.cpp.o"
+  "traininception.pdb"
+  "traininception"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/traininception.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
