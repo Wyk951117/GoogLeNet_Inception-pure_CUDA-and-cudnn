e3ed343d52b50b88bb2b4d4c9412a25b44e66f53
Yikun Wang <yw2227@en-openmpi04.ece.cornell.edu>
2019-05-12 01:34:25 -0400

    partially complete of maxpooling

--- pure-CUDA/layer.cu
+++ pure-CUDA/layer.cu
@@ -507,3 +507,43 @@ __global__ void bp_bias_conv(float *bias, float *d_preact, const int size, const
 		//atomicAdd(&bias[i1], dt * d_preact[i1][i2][i3] / d);  // what is this d for?
 	}
 }
+
+/**name: fp_maxpool
+ * function: forward pass of maxpooling layer
+ * @param 
+ */
+__global__ void fp_maxpool(float *input, float* output, const int kernel_size,
+						const int size, const int n_size, const int n_channel, bool SAME)
+{
+	const int pos = blockIdx.x * blockDim.x + threadIdx.x;
+	const int totalPos = blockDim.x * gridDim.x;
+	const int N = kernel_size * kernel_size * size * size * in_channel * out_channel;  // total number of connections in this convolution
+	const int weight_channel = in_channel * out_channel;  // actual number of channels of weight matrix
+	const int padding = (kernel_size - 1) / 2;  // number of padding for both ends
+
+	// distribute certain number of connections to each thread regardless of detailed position and shape
+	for(int n = N * pos / totalPos; n < N * (pos+1) / totalPos; n++){
+		int idx = n;
+		const int i_kernel_row = ((idx /= 1	) % kernel_size);  
+		const int i_kernel_col = ((idx /= kernel_size	) % kernel_size);
+		const int i_channel = ((idx /= kernel_size	) % weight_channel);
+		const int i_row = ((idx /= weight_channel	) % n_size);
+		const int i_col = ((idx /= size	) % n_size);
+
+		// corresponding position of the input matrix and size of output matrix
+		if (SAME){ // SAME padding scheme implemented
+			const int input_row = i_kernel_row + i_row - padding;
+			const int input_col = i_kernel_col + i_col - padding;
+		}
+		else{
+			const int input_row = i_kernel_row + i_row;
+			const int input_col = i_kernel_col + i_col;
+		}
+		if(input_row >= 0 && input_col < size && input_col >=0 && input_col < size){
+			atomicAdd(output[((i_channel % out_channel) * n_size + i_col) * n_size + i_row], 
+						weight[(i_channel * kernel_size + i_kernel_col) * kernel_size + i_kernel_row] 
+						* input[((i_channel % in_channel) * size + input_col) * size + input_row]);
+		}
+	}
+}
+
