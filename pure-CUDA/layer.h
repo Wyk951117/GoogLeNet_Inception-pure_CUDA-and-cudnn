152b4cf4ed28a2c1062b9e47fdaf32b8c13351b7
zrqohbug <zrqohbug@126.com>
2019-05-12 03:31:48 -0400

    Update layer.h

--- pure-CUDA/layer.h
+++ pure-CUDA/layer.h
@@ -47,6 +47,7 @@ __global__ void fp_conv(float* output, float* input, float* weight, const int ke
 __global__ void fp_bias_conv(float* preact, float* bias, const int size, const int n_channel);
 __global__ void fp_preact_fc(float* input, float* preact, float* weight, const int size, const int in_channel, const int out_channel);
 __global__ void fp_bias_fc(float *preact, float *bias, const int n_channel);
+__global__ void fp_maxpool(float* output, float* input, const int kernel_size, const int size, const int n_size, const int in_channel, bool SAME);
 // Back propagation kernels
 __global__ void decat(float* input, float* output1, float* output2, float* output3, float* output4, const int size, const int out_channel1, const int out_channel2, const int out_channel3, const int out_channel4);
 __global__ void bp_weight_fc(float *d_weight, float *d_preact, float *p_output, const int size, const int in_channel, const int out_channel);
@@ -55,3 +56,4 @@ __global__ void bp_output_conv(float *d_output, float *n_weight, float *nd_preac
 __global__ void bp_preact_conv(float *d_preact, float *d_output, float *preact, const int size, const int n_channel);
 __global__ void bp_weight_conv(float* d_weight, float* d_preact, float* p_output, const int kernel_size, const int size, const int n_size, const int in_channel, const int out_channel, bool SAME);
 __global__ void bp_bias_conv(float *bias, float *d_preact, const int size, const int n_channel); 
+__global__ void bp_maxpool(float* d_preact, float* preact, float* p_output, const int kernel_size, const int size, const int n_size, const int in_channel, bool SAME);
